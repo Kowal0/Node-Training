3d2b1c156c9ae57fe4c55827ae07719027743e63
Krzysztof Kowalski <krzysztof.kowalski@getresponse.com>
2018-11-12 23:02:27 +0100

    remove pug and handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,21 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
 
-const rootDir = require("../utils/path");
+const express = require('express');
+
+const rootDir = require('../util/path');
 const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
+router.get('/', (req, res, next) => {
   const products = adminData.products;
   res.render('shop', {
     prods: products,
-    docTitle: 'Shop'
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
   });
 });
 
