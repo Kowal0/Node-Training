25a527cc1f2bfbdd59ecd48dbba62f22f09e8ce2
Krzysztof Kowalski <krzysztof.kowalski@getresponse.com>
2018-11-12 20:03:19 +0100

    add ejs dynamic content

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
